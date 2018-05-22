b8a693f70adf6a022a786d3c30f2e1e763ad23a9
beranemo <beranemo@gmail.com>
2018-05-22 23:15:24 +0800

    complet the create todo

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,6 +2,19 @@ class TodosController < ApplicationController
   
   def index
     @todos = Todo.all
+    @todo = Todo.new
   end
+
+  def create
+    @todo = Todo.new(todo_params)
+    @todo.save
+    
+    redirect_to todos_path
+  end
+  
+  def todo_params
+    params.require(:todo).permit(:title)
+  end
+
   
 end
